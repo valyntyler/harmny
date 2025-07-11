29b6985b01534787c6baa309deb0a1bb62133d94
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-12 00:37:14 +0300

    feat: impl From<NoteName> for AbsolutePitch

--- src/pitch/absolute_pitch/mod.rs
+++ src/pitch/absolute_pitch/mod.rs
@@ -1,1 +1,17 @@
+use crate::note::note_name::NoteName;
+
 pub struct AbsolutePitch(u8);
+
+impl From<NoteName> for AbsolutePitch {
+    fn from(value: NoteName) -> Self {
+        Self(match value {
+            NoteName::C => 0,
+            NoteName::D => 2,
+            NoteName::E => 4,
+            NoteName::F => 5,
+            NoteName::G => 7,
+            NoteName::A => 9,
+            NoteName::B => 11,
+        })
+    }
+}
